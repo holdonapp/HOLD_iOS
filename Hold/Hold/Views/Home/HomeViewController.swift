f329a1f11cbf5971e50c7b3ce2c22ba0dbe4e8f5
BriizeApp <briizebeauty@gmail.com>
2018-12-15 21:05:37 -0800

    Update Fade Away

--- Hold/Hold/Views/Home/HomeViewController.swift
+++ Hold/Hold/Views/Home/HomeViewController.swift
@@ -194,7 +194,7 @@ extension HomeViewController {
     
     private func startImageDetailTimer() {
         self.imageDetailTimer = Timer.scheduledTimer(
-            withTimeInterval: 4.0,
+            withTimeInterval: 2.0,
             repeats: false,
             block: { [weak self] (_) in
                 self?.layoutImageDetailAttributes()
@@ -204,14 +204,16 @@ extension HomeViewController {
     
     private func startImageDetailVanishTimer() {
         self.imageDetailVanishTimer = Timer.scheduledTimer(
-            withTimeInterval: 11.0,
+            withTimeInterval: 10.0,
             repeats: false,
             block: { [weak self] (_) in
                 DispatchQueue.main.async {
                     UIView.animate(
                         withDuration: 2.0,
                         animations: {
-                            self?.imageDetailsView.alpha = 0.0
+                            if self?.detailsAreOpen == false {
+                                self?.imageDetailsView.alpha = 0.0
+                            }
                     })
                 }
         })
@@ -396,7 +398,7 @@ extension HomeViewController {
     
     @objc private func openRightMenu() {
         DispatchQueue.main.async {
-  
+            
         }
     }
     
